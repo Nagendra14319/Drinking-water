e45ab2e0a735f93f8022929a60fd2474a0f7babb
Nagendra14319 <nagendravarma1315@gmail.com>
2025-03-23 14:10:07 +0530

    Delete CSP WEB directory

--- CSP WEB/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("submit-btn").addEventListener("click", function() {
-    let ph = document.getElementById("ph").value;
-    let turbidity = document.getElementById("turbidity").value;
-    let tds = document.getElementById("tds").value;
-    let temperature = document.getElementById("temperature").value;
-
-    alert("Data Submitted Successfully:\n" +
-        "pH: " + ph + "\nTurbidity: " + turbidity + "\nTDS: " + tds + "\nTemperature: " + temperature);
-});
